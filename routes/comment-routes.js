0ce62f3f82d8f9861e78ebc62f2b2aae0af5aacc
gelong25 <hayaon0603@naver.com>
2024-11-25 18:07:35 +0900

    [chore] 최신 코드 반영

--- routes/comment-routes.js
+++ routes/comment-routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { makeComment, getComments, deleteComment, editComment } from '../controllers/comment-controller.js';
+import { makeComment, getComments, deleteComment, editComment } from '../controllers/comment-controllers.js';
 
 const router = express.Router();
 
