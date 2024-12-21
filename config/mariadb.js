ecd5fbf44a6a8d4f769d8bc836c3881fe1e77fb8
groom <hayaon0603@naver.com>
2024-12-21 23:51:14 +0900

    [chore] dotenv 모듈 추가로 환경 변수 설정 적용

--- config/mariadb.js
+++ config/mariadb.js
@@ -1,13 +1,14 @@
 import maria from 'mysql'; 
+import dotenv from 'dotenv'; 
+
+dotenv.config();
 
 const connection = maria.createConnection({
-    host : process.env.DB_HOST,
-    port : process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 });
 
-export default connection;
-
-
+export default connection; 
