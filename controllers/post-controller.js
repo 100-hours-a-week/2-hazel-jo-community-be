522c02b1a88fcd9184ce99fd8b415c366f03f599
groom <hayaon0603@naver.com>
2025-01-03 02:18:00 +0900

    [refactor] 데이터베이스 작업 로직을 post 모델로 분리

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,87 +1,31 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import fs from 'fs';
-import multer from 'multer';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const postsFilePath = path.join(__dirname, '../data/posts.json');
-
-// JSON 파일에서 게시글 데이터 불러오는 함수 
-export const loadPostData = () => {
-    try {
-        const data = fs.readFileSync(postsFilePath, 'utf8');
-        return JSON.parse(data);
-    } catch (error) {
-        console.error('게시글 데이터 로드 중 오류:', error.message);
-        return {
-            posts: []
-        };
-    }
-};
-
-// JSON 파일에 게시글 데이터 저장하는 함수 
-export const savePostData = (posts) => {
-    try {
-        fs.writeFileSync(postsFilePath, JSON.stringify(posts, null, 2));
-    } catch (error) {
-        console.error('게시글 저장 중 오류:', error.message);
-    }
-};
-
-// multer 설정
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/posts/');  // 업로드 디렉토리 설정
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname);
-    }
-});
-
-const upload = multer({ storage: storage });
+import { 
+    createPost,
+    updatePost, 
+    deletePostById, 
+    deleteCommentByPostId,
+    getPosts, 
+    getPostById, 
+    manageLike, 
+    likePostById,
+    updateView,
+    getCommentCount, 
+} from '../models/post-model.js'; 
 
 // 게시글 작성 makePost
-export const makePost = (req, res) => {
+export const makePost = async(req, res) => {
+    const userId = req.session?.user?.userId; 
+    const { title, content } = req.body;
+    const image = req.file ? `/uploads/posts/${req.file.filename}` : null;     
+    if(!title || !content) {
+        return res.status(400).json({
+            message: '게시글 제목과 내용을 입력해주세요.'
+        });
+    }
     try {
-        const { user_id, title, content, date, profileImg } = req.body;
-        let imagePath = '';
-
-        if(req.file) {  
-            imagePath = '/uploads/posts/' + req.file.filename;
-        }
-
-        if(!title || !content) {
-            return res.status(400).json({
-                message: '게시글 제목과 내용을 입력해주세요.'
-            });
-        }
-
-        const postsData = loadPostData();
-        const newPost = {
-            // 게시글이 있는 경우 마지막 게시글의 post_id + 1 / 게시글이 없는 경우 1
-            post_id: postsData.posts.length > 0 
-                ? postsData.posts[postsData.posts.length - 1].post_id + 1 
-                : 1,
-            user_id,
-            profileImg,
-            title,
-            content,
-            date: date || new Date().toLocaleString(),
-            image: imagePath,
-            like: 0,
-            likes: [],
-            view: 0,
-            comment: 0
-        }; 
-            
-        postsData.posts.push(newPost);
-        savePostData(postsData); 
-        
+        const result = await createPost({ userId, title, content, image }); 
         return res.status(201).json({
             message: '게시글이 성공적으로 작성되었습니다.',
-            post: newPost
+            postId: result.insertId, 
         });
     } catch (error) {
         return res.status(500).json({
@@ -92,58 +36,28 @@ export const makePost = (req, res) => {
 }
 
 // 게시글 수정 editPost
-export const editPost = (req, res) => {
+export const editPost = async (req, res) => {
+    const { postId } =req.params; 
+    const { title, content } = req.body;
+    const userId = req.session.user.userId;
+    const image = req.file ? `/uploads/posts/${req.file.filename}` : null; 
+    
+    if (!title || !content) {
+        return res.status(400).json({
+            message: "제목과 내용을 입력해주세요."
+        });
+    }
     try {
-        const { postId } = req.params;
-        const userId = req.session.user.userId;
-        const { title, content } = req.body;
-        
-        if (!title || !content) {
-            return res.status(400).json({
-                message: "제목과 내용을 입력해주세요."
+        await vertifyWriter(postId, userId); 
+        const result = await updatePost({ postId, title, content, image, userId }); 
+        if(result.affectedRows === 0) {
+            return res.status(403).json({
+                message: "게시글을 수정할 권한이 없습니다."
             });
         }
-
-        const posts = loadPostData();
-        const postIndex = posts.posts.findIndex(post => post.post_id === Number(postId));
-
-        if(postIndex === -1) {
-            return res.status(404).json({
-                message: "게시글을 찾을 수 없습니다." 
-            });
-        }
-
-        // 게시글 작성자와 수정 요청자가 다른 경우 
-        if(Number(posts.posts[postIndex].user_id) !== userId) {
-            return res.status(401).json({
-                message: "해당 게시글 수정 권한이 없습니다."
-            });
-        }
-
-        // multer 파일 업로드
-        let updatedImagePath = posts.posts[postIndex].image;  
-        if(req.file) {
-            updatedImagePath = '/uploads/posts/' + req.file.filename;
-        }
-
-        posts.posts[postIndex] = {
-            ...posts.posts[postIndex],
-            title,
-            content,
-            image: updatedImagePath 
-        };
-
-        // json 파일에 수정된 게시글 저장 
-        try {
-            savePostData(posts);
-            return res.status(200).json({
-                message: "게시글이 수정됐습니다."
-            });
-        } catch (error) {
-            return res.status(500).json({
-                message: "게시글 수정에 실패했습니다."
-            });
-        }
+        return res.status(200).json({
+            message: "게시글이 수정됐습니다."
+        });        
     } catch (error) {
         return res.status(500).json({
             message: "게시글 수정에 실패했습니다.",
@@ -153,31 +67,20 @@ export const editPost = (req, res) => {
 }
 
 // 게시글 삭제 deletePost 
-export const deletePost = (req, res) => {
+export const deletePost = async (req, res) => {
     const { postId } = req.params;
     const userId = req.session.user.userId; 
-    
-    const posts = loadPostData();
-    const postIndex = posts.posts.findIndex(post => post.post_id === Number(postId));
-    
-    if (postIndex === -1) {
-        return res.status(404).json({
-            message: "게시글을 찾을 수 없습니다."
-        });
-    }
-
-    // 게시글 작성자와 삭제 요청자가 다른 경우 : user_id를 숫자로 변환하여 비교
-    if (Number(posts.posts[postIndex].user_id) !== userId) {
-        return res.status(401).json({
-            message: "해당 게시글 삭제 권한이 없습니다."
-        });
-    }
-
-    // 게시글 삭제 
-    posts.posts.splice(postIndex, 1);
 
     try {
-        savePostData(posts);
+        await vertifyWriter(postId, userId); 
+        await deleteCommentByPostId(postId);
+
+        const result = await deletePostById(postId); 
+        if(result.affectedRows === 0) {
+            return res.status(403).json({
+                message: "게시글을 삭제할 권한이 없습니다."
+            });
+        }        
         return res.status(200).json({
             message: "게시글이 삭제되었습니다."
         });
@@ -185,208 +88,156 @@ export const deletePost = (req, res) => {
         return res.status(500).json({
             message: "게시글 삭제에 실패했습니다."
         });
-    }
-     
+    }  
 }
 
 // 게시글 목록 조회 posts
-export const posts = (req, res) => {
-    const posts = loadPostData();
-
-    return res.status(200).json({
-        posts: posts.posts,
-        message: "게시글 목록이 조회됐습니다."
-    });
-    
-}
+export const posts = async (req, res) => {
+    try {
+        const posts = await getPosts();
+        return res.status(200).json({
+            posts, 
+            message: "게시글 목록이 조회됐습니다."
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "게시글 목록 조회에 실패했습니다.",
+            error: error.message,
+        });
+    };  
+};
 
 // 게시글 상세 조회 postDetail 
-export const postDetail = (req, res) => {
+export const postDetail = async (req, res) => {
     const { postId } = req.params;
-    const posts = loadPostData();
-    const post = posts.posts.find(post => post.post_id === Number(postId));
-
-    if(!post) {
-        return res.status(404).json({
-            message: "게시글을 찾을 수 없습니다."
+    try {
+        // 조회수 증가 
+        await updateView(postId); 
+        
+        const post = await getPostById(postId); 
+        if(!post) {
+            return res.status(404).json({
+                message: "게시글을 찾을 수 없습니다."
+            });
+        }
+        
+        return res.status(200).json({
+            post: {
+                post_id: post.post_id,
+                title: post.title,
+                content: post.content,
+                image: post.image,
+                like: post.like,
+                view: post.view,
+                created_at: post.created_at,
+                commentCount: post.comment_count,
+                author: {
+                    user_id: post.user_id,
+                    nickname: post.author_nickname,
+                    profileImg: post.author_profileImg
+                }
+            },
+            message: "게시글이 상세 조회됐습니다."
         });
-    }
+    } catch (error) {
+        res.status(500).json({
+            message: "게시글 상세 조회에 실패했습니다."
+        });
+   };    
+};
 
-    return res.status(200).json({
-        post,
-        message: "게시글이 상세 조회됐습니다."
-    });
-}
 
+// 게시글 좋아요 관리 likeCount
+export const likePost = async (req, res) => {
+    const { postId } = req.params; 
+    const userId = req.session.user.userId;
+    try {
+        const result = await manageLike(postId, userId);
+
+        if (result.action === 'added') {
+            return res.status(200).json({
+                message: "게시글에 좋아요를 추가했습니다.",
+                details: result,
+            });
+        } else if (result.action === 'removed') {
+            return res.status(200).json({
+                message: "게시글에 좋아요를 취소했습니다.",
+                details: result,
+            });
+        } else {
+            return res.status(400).json({
+                message: "게시글 좋아요 작업이 수행되지 않았습니다.",
+            });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            message: "게시글 좋아요 수 처리에 실패했습니다.",
+        });
+    };
+};
 
 // 게시글 좋아요 수 조회 likeCount
 export const likeCount = async (req, res) => {
+    const { postId } = req.params;
     try {
-        const { postId } = req.params;
-        const userId = req.session.user.userId;
-        console.log("likeCount userId", userId);
-
-        const posts = await loadPostData();
-        const post = posts.posts.find(post => post.post_id === Number(postId));
-
-        if(!post) {
-            return res.status(404).json({
-                message: "게시글을 찾을 수 없습니다."
+        const result = await likePostById(postId); 
+            if (!result) {
+                return res.status(500).json({
+                    message: "게시글 좋아요 수를 조회하는 중 문제가 발생했습니다.",
+                });
+            }
+            if (result.length === 0) {
+                return res.status(404).json({
+                    message: "게시글을 찾을 수 없습니다.",
+                });
+            }
+            return res.status(200).json({
+                message: "게시글 좋아요 수가 조회되었습니다.",
+                like: result[0].like,
             });
-        }
-
-        const isLiked = post.likes.includes(userId);
-        
-        return res.status(200).json({
-            like: post.like,
-            isLiked,
-            message: "게시글 좋아요 수가 조회됐습니다."
-        });
-    } catch (error) {
+        } catch (error) {
         return res.status(500).json({
-            message: "게시글 좋아요 수 조회에 실패했습니다.",
-            error: error.message
-        });
-    }
-
-}
-
-// 게시글 좋아요 추가 likePost
-export const likePost = async (req, res) => {
-    try {
-        const { postId } = req.params;
-
-        // 로그인 한 유저의 userId 
-        const userId = req.session.user.userId;
-    
-        const posts = await loadPostData();
-        const post = posts.posts.find(post => post.post_id === Number(postId));
-
-        if(!post) {
-            return res.status(404).json({
-                message: "게시글을 찾을 수 없습니다."
-            });
-        }
-
-        // 게시글 좋아요 누른 유저의 likes 배열이 없으면 초기화
-        if (!post.likes) {
-            post.likes = [];
-        }
-
-        // 유저가 좋아요를 눌렀는지 확인
-        const isLiked = post.likes.includes(userId);
-
-        // 이미 좋아요를 눌렀으면 좋아요 취소 / 안눌렀으면 좋아요 추가 
-        if(isLiked) {
-            post.likes = post.likes.filter(id => id !== userId);
-            post.like = Math.max(post.like - 1, 0);
-        } else {
-            post.likes.push(userId);
-            post.like += 1;
-        }
-        savePostData(posts);
-
-        return res.status(200).json({
-            message: "게시글 좋아요가 저장됐습니다.",
-            likeCount: post.like,
-            isLiked: !isLiked
-        });
-    } catch (error) {
-        return res.status(500).json({
-            message: "게시글 좋아요 저장에 실패했습니다.",
-            error: error.message
+            message: "게시글 좋아요 수 조회 중 문제가 발생했습니다.",
+            error: error.message,
         });
     }
 }
 
-// 게시글 조회수 viewCount
-export const viewCount = (req, res) => {
+
+// 댓글 수 commentCount
+export const commentCount = async (req, res) => {
+    const { postId } = req.params; 
     try {
-        const { postId } = req.params;
-        const posts = loadPostData();
-        const post = posts.posts.find(post => post.post_id === Number(postId));
-
-        if(!post) {
+        const comments = await getCommentCount(postId);
+        if(!comments) {
             return res.status(404).json({
-                message: "게시글을 찾을 수 없습니다."
-            });
+                message: "댓글을 찾을 수 없습니다."
+            }); 
         }
-
-        post.view += 1;
-        savePostData(posts);
-
-        return res.status(200).json({
-            viewCount: post.view,
-            message: "게시글 조회수가 조회됐습니다."
+        res.status(200).json({
+            commentCount: comments[0].comment_count,
+            message: "댓글 수 조회에 성공했습니다."
         });
     } catch (error) {
-        return res.status(500).json({
-            message: "게시글 조회수 조회에 실패했습니다.",
-            error: error.message
-        });
+        res.status(500).json({
+            message: "댓글 수 조회에 실패했습니다."
+        }); 
     }
 }
 
-// 게시글 댓글 수 updateCommentCount
-export const updateCommentCount = (req, res) => {
-    try {
-        const { postId } = req.params;
-        const { action } = req.body;
-        const posts = loadPostData();
-        const post = posts.posts.find(post => post.post_id === Number(postId));
 
-        if(!post) {
-            return res.status(404).json({
-                message: "게시글을 찾을 수 없습니다."
-            });
-        }
-
-        // 댓글 수 증가/감소 
-        if (action === "add") {
-            post.comment += 1; 
-        } else if (action === "delete") {
-            post.comment = Math.max(post.comment - 1, 0); 
-        } else {
-            return res.status(400).json({
-                message: "유효하지 않은 액션입니다. 'add' 또는 'delete'를 사용하세요."
-            });
-        }
-        savePostData(posts);
-
-        return res.status(200).json({
-            commentCount: post.comment,
-            message: "게시글 댓글 수가 조회됐습니다."
-        });
-    } catch (error) {
-        return res.status(500).json({
-            message: "게시글 댓글 수 조회에 실패했습니다.",
-            error: error.message
-        });
+// 게시글 작성자 검증 
+export const vertifyWriter = async (postId, userId) => {
+    const post = await getPostById(postId);
+    if(!post) {
+        throw new Error('게시글을 찾을 수 없습니다');
     }
+    if(post.user_id !== userId) {
+        throw new Error('해당 게시글에 대한 권한이 없습니다.');
+    }
+    return post; 
 }
 
-// 게시글 댓글 수 commentCount
-export const commentCount = (req, res) => {
-    try {
-        const { postId } = req.params;
-        const posts = loadPostData();
-        const post = posts.posts.find(post => post.post_id === Number(postId));
 
-        if(!post) {
-            return res.status(404).json({
-                message: "게시글을 찾을 수 없습니다."
-            });
-        }
-        return res.status(200).json({
-            commentCount: post.comment,
-            message: "게시글 댓글 수가 조회됐습니다."
-        });
 
-    } catch (error) {
-        return res.status(500).json({
-            message: "게시글 댓글 수 조회에 실패했습니다.",
-            error: error.message
-        });
-    }
-}   
 
---
+++ models/post-model.js
@@ -0,0 +1,221 @@
+import connection from '../config/mariadb.js';
+
+// 게시글 생성 createPost 
+export const createPost = async ({ userId, title, content, image }) => {
+    return new Promise((resolve, reject) => {
+        const query = `
+        INSERT INTO post (user_id, title, content, image, created_at)
+        VALUES (?, ?, ?, ?, NOW())
+        `;
+        const params = [userId, title, content, image];
+        connection.query(query, params, (err, results) => {
+            if(err) {
+                return reject(err);
+            }
+            resolve(results);
+        });
+    });
+};
+
+// 게시글 수정 updatePost
+export const updatePost = async ({ postId, title, content, image, userId }) => {
+    return new Promise((resolve, reject) => {
+        const query = `
+        UPDATE post SET title = ?, content = ?, image = ?
+        WHERE post_id = ? AND user_id = ?
+        `;
+        const params = [title, content, image, postId, userId || null];
+        connection.query(query, params, (err, results) => {
+            if(err) {
+                return reject(err);
+            }
+            resolve(results); 
+        });
+    });
+}; 
+
+// 게시글 삭제 deletePostById
+export const deletePostById = async (postId) => {
+    return new Promise((resolve, reject) => {
+        const query = `DELETE FROM post WHERE post_id =?`;
+        connection.query(query, [postId], (err, results) => {
+            if(err) {
+                return reject(err);
+            }
+            resolve(results); 
+        });
+    });
+};
+
+// 게시글 관련 댓글 삭제 deleteCommentByPostId
+export const deleteCommentByPostId = async (postId) => {
+    return new Promise((resolve, reject) => {
+        const query = `DELETE FROM comment WHERE post_id = ?`;
+        connection.query(query, [postId], (err, results) => {
+            if(err) {
+                return reject(err);
+            }
+            resolve(results);
+        })
+    })
+}
+
+// 게시글 목록 조회 getPosts
+export const getPosts = async () => {
+    return new Promise((resolve, reject) => {
+        const query = `
+            SELECT 
+                p.*,
+                u.nickname,
+                u.profileImg,
+                DATE_FORMAT(p.created_at, '%Y-%m-%d %H:%i:%s') as created_at
+            FROM post p
+            LEFT JOIN users u ON p.user_id = u.user_id
+            ORDER BY p.created_at ASC
+        `; 
+        connection.query(query, (err, results) => {
+            if(err) {
+                return reject(err);
+            }
+            const posts = results.map(post => {  
+                return {
+                    post_id: post.post_id,
+                    user_id: post.user_id,
+                    title: post.title || '',
+                    content: post.content || '',
+                    image: post.image || null,
+                    view: post.view || 0,
+                    like: post.like || 0,
+                    created_at: post.created_at,
+                    nickname: post.nickname || '작성자 없음',
+                    profileImage: post.profileImg || null,
+                    comment: post.comment_count || 0
+                };
+            });
+            resolve(posts); 
+        });
+    });
+};
+
+// 게시글 상세 조회 getPostById
+export const getPostById = async (postId) => {
+    return new Promise((resolve, reject) => {
+        const query = `
+            SELECT p.*,
+                u.nickname AS author_nickname,
+                u.profileImg AS author_profileImg
+            FROM post p 
+            LEFT JOIN users u ON p.user_id = u.user_id
+            WHERE p.post_id = ?
+            `; 
+        connection.query(query, [postId], (err, results)=> {
+            if(err) {
+                return reject(err);
+            }
+            resolve(results[0]); 
+        });
+    });
+};
+
+// 좋아요 수 manageLike
+export const manageLike = async (postId, userId) => {
+    return new Promise((resolve, reject) => {
+        connection.beginTransaction(err => {
+            if(err) {
+                return reject(err);
+            }
+            // 좋아요 상태 확인 
+            const userCheckQuery = `SELECT * FROM likes WHERE post_id = ? AND user_id = ?`;
+            const params = [postId, userId];
+            connection.query(userCheckQuery, params, (err, results) => {
+                if(err) {
+                    return connection.rollback(() => reject(err));
+                }
+                // LIKES 좋아요 삭제 
+                if(results.length > 0) {
+                    const deleteQuery = `DELETE FROM likes WHERE post_id = ? AND user_id = ?`;
+                    connection.query(deleteQuery, params, (err, deleteResults) => {
+                        if(err) {
+                            return connection.rollback(() => reject(err));
+                        }
+                        // POST 좋아요 수 감소 
+                        const decrementQuery = `UPDATE post SET \`like\` = \`like\` - 1 WHERE post_id = ?`;
+                        connection.query(decrementQuery, [postId], (err, decrementResults) => {
+                            if (err) {
+                                return connection.rollback(() => reject(err));
+                            }
+                            // 커밋
+                            connection.commit(err => {
+                                if (err) {
+                                    return connection.rollback(() => reject(err));
+                                }
+                                resolve({ action: 'removed', deleteResults, decrementResults });
+                            });
+                        });
+                    });
+                } else {
+                    // LIKES 좋아요 추가 
+                    const insertQuery = `INSERT INTO likes (post_id, user_id) VALUES (?, ?)`;
+                    connection.query(insertQuery, params, (err, insertResults) => {
+                        if (err) {
+                            return connection.rollback(() => reject(err));
+                        }
+                        // POST 좋아요 수 증가 
+                        const incrementQuery = `UPDATE post SET \`like\` = \`like\` + 1 WHERE post_id = ?`;
+                        connection.query(incrementQuery, [postId], (err, incrementResults) => {
+                            if (err) {
+                                return connection.rollback(() => reject(err));
+                            }
+                            // 커밋
+                            connection.commit(err => {
+                                if (err) {
+                                    return connection.rollback(() => reject(err));
+                                }
+                                resolve({ action: 'added', insertResults, incrementResults });
+                            });
+                        });
+                    });
+                }
+            });
+        });
+    });
+};
+
+// 좋아요 수 
+export const likePostById = async (postId) => {
+    return new Promise((resolve, reject) => {
+        const query = `SELECT \`like\` FROM post WHERE post_id = ?`;
+        connection.query(query, [postId], (err, results) => {
+            if(err) {
+                return reject(err);
+            }
+            resolve(results); 
+        });
+    }); 
+};
+
+// 조회 수 updateView
+export const updateView = async (postId) => {
+    return new Promise((resolve, reject) => {
+        const query = `UPDATE post SET view = view + 1 WHERE post_id = ?`;
+        connection.query(query, [postId], (err, results) => {
+            if(err) {
+                return reject(err);
+            }
+            resolve(results); 
+        });
+    }); 
+};
+
+// 댓글 수 getCommentCount
+export const getCommentCount = async (postId) => {
+    return new Promise((resolve, reject) => {
+        const query = `SELECT comment_count FROM post WHERE post_id = ?`;
+        connection.query(query, [postId], (err, results) => {
+            if(err) {
+                return reject(err);
+            }
+            resolve(results);
+        });
+    });
+};
