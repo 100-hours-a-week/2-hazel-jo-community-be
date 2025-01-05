32f1e1d76aaae307f5dfdbc18d0ce12ea25b132d
groom <hayaon0603@naver.com>
2025-01-06 01:20:30 +0900

    [fix] 서버버 외부 네트워크 접근 가능하도록 수정

--- index.js
+++ index.js
@@ -67,6 +67,6 @@ app.use('/comments', commentRoutes);
 app.use('/auth', authRoutes); 
 
 const PORT = process.env.PORT || 5001; 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
